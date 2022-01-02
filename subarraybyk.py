75bff13dc96380265ef1981c944288609eb5b684
Gaurav Chaudhary <75659458+Gaurav-AL@users.noreply.github.com>
2022-01-02 21:41:44 +0530

    O(n) HashMap utilized recommended method for subarray sum type ques.

--- subarraybyk.py
+++ subarraybyk.py
@@ -1,22 +1,19 @@
+from collections import defaultdict
 arr = [int(x) for x in input("Enter array :").split(" ")]
 k = int(input("Enter the K value :"))
-lookup = {}
-tsum = 0
+lookup = defaultdict(int)
+lookup[0] = 1
+tsum,rem,total = 0,0,0
 for x in range(len(arr)):
     tsum += arr[x]
-    lookup[x] = tsum%k
-count = 0
-lis = []
-key = 0
-for k,v in lookup.items():
-    if(v == 0):
-        count += 1
-        continue
-    if(v in lis):
-        key = lis.index(v)
-        print(lis)
-        if(abs(k - key) > 1):
-            count += 1
-    lis.append(v)
-print(count,lis)
+    rem = tsum % k
+    if(rem < 0):
+        rem += k
+    total += lookup[rem]
+    lookup[rem] += 1
+
+print(lookup,total)
+
+
+
 
