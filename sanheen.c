38cc92788be78dea02a04afe7caeac7e3a511778
Gaurav Chaudhary <75659458+Gaurav-AL@users.noreply.github.com>
2022-01-07 15:33:35 +0530

    Create sanheen.c

---
+++ sanheen.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    char not;
+    not = !45;
+    printf("%u",not);
+}
+// it signifies that any character of 'char' type could have char and int possible type only, %f will give error
+
+outut : 0 for %d,%u
+        nothing for %c
