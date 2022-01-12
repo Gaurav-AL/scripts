76b0de4216fb00d2270eddcd290498b32385ae04
Gaurav Chaudhary <reverse454@gmail.com>
2022-01-12 18:04:11 +0530

    Trapping of rain Water O(n + n +n) = O(n) solution

---
+++ trapping_of_rain_water.py
@@ -0,0 +1,15 @@
+nums = [int(x) for x in input("Enter the Block Heights :").split(" ")]
+max_left,max_right,trapped_water = [nums[0]],[nums[len(nums)-1]],0
+
+for x in range(1,len(nums)):
+    max_left.append(max(max_left[x-1],nums[x]))
+
+for x in reversed(range(len(nums))):
+    
+    max_right.append(max(max_right[len(nums)-x-1],nums[x-1]))
+    
+print(max_left,max_right)
+for x in range(1,len(nums)-1):
+    trapped_water += min(max_left[x-1],max_right[x+1]) - nums[x]
+    
+print(trapped_water)
